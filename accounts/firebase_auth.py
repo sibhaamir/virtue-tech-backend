73a0a7a655ee94f68d69ae1f8bd025d3e71ad737
sibhaamir <sibhaaamir@gmail.com>
2026-03-12 03:40:10 +0500

    fix firebase auth for railway deployment

--- accounts/firebase_auth.py
+++ accounts/firebase_auth.py
@@ -1,18 +1,28 @@
 import firebase_admin
 from firebase_admin import auth as firebase_auth, credentials
-from django.conf import settings
 from rest_framework import authentication, exceptions
 from .models import User
+import os
+import json
 
-# Initialize Firebase Admin
+# Initialize Firebase Admin using ENV variable
 if not firebase_admin._apps:
-    cred = credentials.Certificate(settings.FIREBASE_KEY_PATH)
+    firebase_key = os.environ.get("FIREBASE_KEY")
+
+    if not firebase_key:
+        raise Exception("FIREBASE_KEY environment variable not set")
+
+    cred_dict = json.loads(firebase_key)
+    cred = credentials.Certificate(cred_dict)
+
     firebase_admin.initialize_app(cred)
 
 
 class FirebaseAuthentication(authentication.BaseAuthentication):
+
     def authenticate(self, request):
         auth_header = authentication.get_authorization_header(request).split()
+
         if not auth_header or auth_header[0].lower() != b"bearer":
             return None
 
@@ -30,7 +40,7 @@ class FirebaseAuthentication(authentication.BaseAuthentication):
         user, created = User.objects.get_or_create(
             firebase_uid=uid,
             defaults={
-                "username": email.split("@")[0],
+                "username": email.split("@")[0] if email else uid,
                 "email": email,
                 "first_name": name.split(" ")[0] if name else "",
                 "last_name": " ".join(name.split(" ")[1:]) if name else "",
